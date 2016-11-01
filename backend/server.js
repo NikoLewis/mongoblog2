4b26f25c8e7413838bd5157000b0ac18bea381de
Niko Lewis <BellaLewis99@gmail.com>
2016-11-01 15:28:16 -0400

    Second commit- working mongo express node setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,40 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-//Connect to database and start server:
-//(whatever we put after 'localhost/' will automatically be the name of database)
-mongoose.connect('mongodb://localhost/blog-app');
-//Store connection as variable
+const mongoose = require('mongoose');
+const postModel = require('./posts/posts-model')
+const Post = mongoose.model('Post')
+
+mongoose.connect('mongodb://localhost/blog-database');
+
 const db = mongoose.connection;
-//Start the server after successful database connection:
-db.on('open', () => {
-  app.listen(4444, () => {
-    console.log('App listening on port 4444');
-  });
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
+
+app.get('/posts',(req,res)=>{
+	res.send('hey from the posts page')
+})
+
+db.on('open', () => {
+	console.log('db connection opened');
+	app.listen(3000, function () {
+	  console.log('Example app listening on port 3000!');
+	  Post.create({title: 'test post 1'}, (err,data)=>{
+	  	if(err) console.log('Error with database');
+	  	else console.log('Post created!')
+	  })
+	  //finds all posts and logs to console
+	  Post.find({},(err,data)=>{
+	  	console.log('database found!', data)
+	  })
+	  //Find all posts that match specific title
+      Post.find({title: 'find this title'}, (err, data) => {
+        console.log('Database data found!', data);
+      })
+	});
+})
+
+db.on('error', ()=>{
+	console.log('error in db connection!')
+})xxxx
